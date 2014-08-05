0ce573ad0e4d1b17fe8a33b7dbbc35478ec4559f
mzaiss <altustro@gmail.com>
2014-08-05 17:35:45 +0200

    added gpl

--- BATCH_Z_pSL.m
+++ BATCH_Z_pSL.m
@@ -1,7 +1,24 @@
+%% analytic Z-spectra
+%   Date: 2014/08/01 
+%   Version for CEST-sources.de
 %   Author: Moritz Zaiss  - m.zaiss@dkfz.de
-%   Date: 2014/07/25 
-%   Version for cest sources
-  
+%   CEST sources  Copyright (C) 2014  Moritz Zaiss
+%   **********************************
+%   This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+%    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+%    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%   **********************************
+%
+%   --SHORT  DOC--
+%   The parameter struct P contains all system and sequence parameters.
+%   Z_cw_2pool(P) calculates the Z-spectrum and returns it as a vector.
+%
+%   --references--:
+%               2-pool-cw:      Zaiss et al. JCP, Zaiss and Bachert NBM      
+%               3-pool-cw:      Zaiss et al. NBM    
+%               2-pool-plsd SL: Roelloffs et al. NBM 
 clear all
 %% SETUP
  
